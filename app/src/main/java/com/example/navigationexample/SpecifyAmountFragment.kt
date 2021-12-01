fbab8139ada285b5ac7026c1e92ef30f4722fc71
dfchaves <davidchaves@tyba.com.co>
2021-11-30 20:08:11 -0500

    Navigation initial

---
+++ app/src/main/java/com/example/navigationexample/SpecifyAmountFragment.kt
@@ -0,0 +1,46 @@
+package com.example.navigationexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SpecifyAmountFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+
+    var navController: NavController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.send_btn -> navController?.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+            R.id.cancel_btn -> activity?.onBackPressed()
+        }
+    }
+}
